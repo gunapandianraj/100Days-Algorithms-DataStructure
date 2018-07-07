4b8ac2a0a6ae4e919e3b86686e92847f4886dae8
Gunapandian <gunapandianraj@gmail.com>
2018-07-07 23:06:52 +0530

    Base setup done

---
+++ Algorithms&Datastructures/Algorithms&Datastructures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithms&Datastructures
+//
+//  Created by Gunapandian on 07/07/18.
+//  Copyright © 2018 Gunapandian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
